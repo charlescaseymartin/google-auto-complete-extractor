0f9df45b4216c2e38ae0624f27ff940266466218
charlescaseymartin <charlescaseymartin@gmail.com>
2024-05-02 15:16:25 +0200

    added better argument parsing

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import sys
 import os
-from selenium import webdriver
-from common.constants import REMOTE_WEBDRIVER
-from common.utils import wait_for_selenium_to_start
-from scrapper import ExtractAutoSuggestions
+import argparse
 
-def create_browser_instance():
-    options = webdriver.FirefoxOptions()
-    options.add_argument("--no-sandbox")
-    options.add_argument("--headless")
-    options.add_argument("--disable-dev-shm-usage")
-    return webdriver.Remote(REMOTE_WEBDRIVER, options=options)
 
 def invalid_keyword_file():
     print('Invalid keyword file. Read help menu for more')
     sys.exit(1)
 
-def load_keywords(key_path:str):
-    if len(key_path) > 0 and os.path.isfile(key_path) == False:
+
+def load_keyword_file(key_path: str):
+    if len(key_path) > 0 and os.path.isfile(key_path) is False:
         invalid_keyword_file()
     keywords = []
     with open(key_path, 'r+') as keyword_file:
@@ -27,24 +19,16 @@ def load_keywords(key_path:str):
         invalid_keyword_file()
     return keywords
 
-def usage():
-    print('''This is the help menu for Google Auto-complete Extrator!
-
-    -h          Shows this menu
-
-    -k [path]   A text file (.txt) containing coma separated list of
-                keywords to generate topic authority''')
 
 if __name__ == '__main__':
-    args = sys.argv
-    if len(args) > 1 and args[1] == '-k' and 2 < len(args):
-        keywords = load_keywords(key_path=args[2])
-        print('[*] Keyword/s loaded')
-        wait_for_selenium_to_start()
-        browser = create_browser_instance()
-        print('[*] Browser instance is ready')
-        scrapper = ExtractAutoSuggestions(browser, keywords)
-        scrapper.run()
-        browser.quit()
-    else:
-        usage()
+    prog = 'Google Suggestions'
+    description = 'This program produces Google auto-complete suggestions of provided keywords'
+    parser = argparse.ArgumentParser(prog=prog, description=description)
+    parser.add_argument('-k', '--keywords', required=False, nargs='*')
+    parser.add_argument('-f', '--filename', required=False, nargs=1)
+    parser.add_argument('-o', '--output', required=False, nargs=1)
+    args = parser.parse_args()
+    keyword_args = args.keywords
+    keyword_file_arg = args.filename[0] if args.filename and len(args.filename) > 0 else None
+    output_file_arg = args.output[0] if args.output and len(args.output) > 0 else None
+    print(f'ARGS:\nkeywords: {keyword_args}\nkeyword file: {keyword_file_arg}\noutput file: {output_file_arg}')

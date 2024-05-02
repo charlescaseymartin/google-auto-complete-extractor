c7ea466009d9deed220a4f49cbca99edd91111ee
charlescaseymartin <charlescaseymartin@gmail.com>
2024-05-03 01:54:54 +0200

    added core functionality to refactor python implementation

--- common/constants.py
+++
@@ -1,5 +0,0 @@
-import os
-
-REMOTE_WEBDRIVER = 'http://selenium:4444/wd/hub'
-root_path = os.getcwd()
-RESULTS_FILE_PATH = f'{root_path}/data/results.json'
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 import os
+import json
 import argparse
+import requests
 
 
 def invalid_keyword_file():
-    print('Invalid keyword file. Read help menu for more')
+    print('Invalid keyword file. Read help(-h/--help) menu for more')
     sys.exit(1)
 
 
@@ -14,24 +16,72 @@ def load_keyword_file(key_path: str):
     keywords = []
     with open(key_path, 'r+') as keyword_file:
         lines = [line.strip().split(',') for line in keyword_file.readlines()]
-        keywords = [keyword.strip() for row in lines for keyword in row if len(keyword) > 1]
+        keywords = [keyword.strip()
+                    for row in lines for keyword in row if len(keyword) > 1]
     if len(keywords) < 1:
         invalid_keyword_file()
     return keywords
 
 
-if __name__ == '__main__':
+def get_all_keywords(direct_keys: [str], key_file_dir: str):
+    keywords = []
+    if direct_keys is None or len(direct_keys) < 1:
+        print(f'get_all_keywords direct_keys: {direct_keys}')
+        print('-k [--keywords] Invalid keywords provided.')
+        sys.exit(1)
+    loaded_file_keywords = load_keyword_file(key_file_dir)
+    keywords.extend(direct_keys)
+    keywords.extend(loaded_file_keywords)
+    return keywords
+
+
+def parse_args():
     prog = 'Google Suggestions'
     description = 'This program produces Google auto-complete suggestions of provided keywords'
+    key_help = 'A list of keywords separated by spaces to get Google Suggestions for'
+    key_file_help = 'A text file of keywords separated by commas to get Google Suggestions for'
+    out_file_help = 'A json file of the Google Suggestions results (default is ./output.json)'
+    default_output_file = os.path.join(os.getcwd(), 'output.json')
+
     parser = argparse.ArgumentParser(prog=prog, description=description)
-    parser.add_argument('-k', '--keywords', required=False, nargs='*')
-    parser.add_argument('-f', '--filename', required=False, nargs=1)
-    parser.add_argument('-o', '--output', required=False, nargs=1)
+    parser.add_argument('-k', '--keywords', required=False, nargs='*', help=key_help)
+    parser.add_argument('-f', '--filename', required=False, nargs=1, help=key_file_help)
+    parser.add_argument('-o', '--output', required=False, nargs=1, help=out_file_help)
+
     args = parser.parse_args()
     keyword_args = args.keywords
-    keyword_file_arg = args.filename[0] if args.filename and len(args.filename) > 0 else None
-    output_file_arg = args.output[0] if args.output and len(args.output) > 0 else None
+    keyword_file_arg = args.filename[0] if args.filename else None
+    output_file_arg = args.output[0] if args.output else default_output_file
+
     if keyword_args is None and keyword_file_arg is None:
         parser.print_help()
         sys.exit(1)
-    print(f'ARGS:\nkeywords: {keyword_args}\nkeyword file: {keyword_file_arg}\noutput file: {output_file_arg}')
+
+    return {
+        "keywords": keyword_args,
+        'keywords_file': keyword_file_arg,
+        'output_file': os.path.join(os.getcwd(), f'{output_file_arg}')
+    }
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    keywords = args.get('keywords')
+    keywords_file = args.get('keywords_file')
+    output_file = args.get('output_file')
+    all_keywords = get_all_keywords(args.get('keywords'), args.get('keywords_file'))
+    results = {}
+    suggestions_url = 'https://www.google.com/complete/search?client=chrome&q='
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0'
+    }
+
+    for keyword in all_keywords:
+        url = f'{suggestions_url}{keyword}'
+        response = requests.get(url, headers=headers)
+        parsed_response = json.loads(response.text)[1]
+        results[keyword] = parsed_response
+
+
+    with open(output_file, 'w+') as output:
+        output.write(json.dumps(results, indent=4))
